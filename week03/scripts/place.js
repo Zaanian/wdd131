1ad6e03358e277480fc8d8952d0ced341914841c
Zaanian <sstauffer1@byupathway.edu>
2025-09-20 09:12:33 -0700

    w3 progress

---
+++ week03/scripts/place.js
@@ -0,0 +1,9 @@
+// Set the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+//get the last modified date
+const lastmodifieddate= document.lastModified
+document.getElementById("lastmodified").textContent = ("Last modified " + lastmodifieddate)
+
+//windchill calculation
+
