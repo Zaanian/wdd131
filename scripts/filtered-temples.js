50dbfcb8db92bba97c64917bfbe76a75ebb8b561
Zaanian <sstauffer1@byupathway.edu>
2025-09-26 19:44:19 -0700

    w4 progress

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,119 @@
+
+// hamburger button for menu in mobile size
+const hamButton = document.querySelector('#menu');
+const mainnav = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hamButton.classList.toggle('show');
+});
+
+
+// Set the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+//get the last modified date
+const lastmodifieddate = document.lastModified
+document.getElementById("lastmodified").textContent = ("Last modified " + lastmodifieddate)
+
+
+//temples data array 
+const temples = [
+    {
+        templeName: "Aba Nigeria",
+        location: "Aba, Nigeria",
+        dedicated: "2005, August, 7",
+        area: 11500,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+        templeName: "Manti Utah",
+        location: "Manti, Utah, United States",
+        dedicated: "1888, May, 21",
+        area: 74792,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+        templeName: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015, June, 7",
+        area: 96630,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+        templeName: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020, May, 2",
+        area: 6861,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+        templeName: "Washington D.C.",
+        location: "Kensington, Maryland, United States",
+        dedicated: "1974, November, 19",
+        area: 156558,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Lima Perú",
+        location: "Lima, Perú",
+        dedicated: "1986, January, 10",
+        area: 9600,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+        templeName: "Mexico City Mexico",
+        location: "Mexico City, Mexico",
+        dedicated: "1983, December, 2",
+        area: 116642,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    {
+        templeName: "Seattle Washington",
+        location: "Seattle, Washington",
+        dedicated: "1980, November, 17-21",
+        area: 110000,
+        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/seattle-washington-temple/seattle-washington-temple-55800.jpg"
+    },
+    {
+        templeName: "Rome Italy",
+        location: "Rome, Italy",
+        dedicated: "2019, March, 10-12",
+        area: 41010,
+        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/rome-italy-temple/rome-italy-temple-2642.jpg",
+    },
+    {
+        templeName: "Portland Oregon",
+        location: "Portland, Oregon",
+        dedicated: "1989, August, 19-21",
+        area: 80500,
+        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/portland-oregon-temple/portland-oregon-temple-37854.jpg",
+    }
+
+    // Add more temple objects here...
+];
+
+//temple cards
+
+function cardTemplate(card) {
+    return `<table>
+    <tr><td>${card.templeName} </td></tr>
+    <tr><td>${card.location} </td></tr>
+    <tr><td>${card.dedicated} </td></tr>
+    <tr><td>${card.area} </td></tr>
+    
+  </table>`
+}
+function renderCards(card) {
+    const html = card.map(cardTemplate);
+    document.querySelector("main").innerHTML = html.join("");
+}
+
+//responsive button
