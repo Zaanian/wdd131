bcdf543847323c9fe04e1c12cccd96b4d7ba63b9
Zaanian <sstauffer1@byupathway.edu>
2025-10-04 14:15:30 -0700

    wk5

---
+++ scripts/form.js
@@ -0,0 +1,54 @@
+
+
+// Set the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+//get the last modified date
+const lastmodifieddate = document.lastModified
+document.getElementById("lastmodified").textContent = ("Last modified " + lastmodifieddate)
+
+
+
+
+
+// Product array
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+
+function productTable (table) {
+    return `
+    <option value="${table.id}">${table.name}</option>`
+}
+
+function addToList (table) {
+    const html = table.map(productTable);
+    document.querySelector("#product-select").innerHTML = html.join("")
+}
+
+
