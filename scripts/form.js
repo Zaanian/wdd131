e84a10831a56ee61c995d0de915a4174a1491f92
Zaanian <sstauffer1@byupathway.edu>
2025-10-04 15:06:23 -0700

    wk5

--- scripts/form.js
+++ scripts/form.js
@@ -13,42 +13,45 @@ document.getElementById("lastmodified").textContent = ("Last modified " + lastmo
 
 // Product array
 const products = [
-  {
-    id: "fc-1888",
-    name: "flux capacitor",
-    averagerating: 4.5
-  },
-  {
-    id: "fc-2050",
-    name: "power laces",
-    averagerating: 4.7
-  },
-  {
-    id: "fs-1987",
-    name: "time circuits",
-    averagerating: 3.5
-  },
-  {
-    id: "ac-2000",
-    name: "low voltage reactor",
-    averagerating: 3.9
-  },
-  {
-    id: "jj-1969",
-    name: "warp equalizer",
-    averagerating: 5.0
-  }
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
 ];
 
 
-function productTable (table) {
+function productTable(table) {
     return `
     <option value="${table.id}">${table.name}</option>`
 }
 
-function addToList (table) {
+function addToList(table) {
     const html = table.map(productTable);
-    document.querySelector("#product-select").innerHTML = html.join("")
+    document.querySelector("#product-select").innerHTML = html.join("");
 }
+
+changeAttr = document.getElementById("product-select")
+
 
 
