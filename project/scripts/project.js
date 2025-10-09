c01c15edbf40ed5eb4f0c8894c573bcaf3461465
Zaanian <sstauffer1@byupathway.edu>
2025-10-09 10:45:53 -0700

    wk6

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -4,3 +4,63 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 //get the last modified date
 const lastmodifieddate = document.lastModified
 document.getElementById("lastmodified").textContent = ("Last modified " + lastmodifieddate)
+
+
+//Object array for rocks
+
+const rocks = [
+    {
+        name: "agate",
+        type: "",
+        image: ""
+
+    },
+    {
+        name: "jasper",
+        type: "",
+        image: ""
+    },
+    {
+        name: "quartz",
+        type: "",
+        image: ""
+    },
+    {
+        name: "granite",
+        type: "",
+        image: ""
+    },
+    {
+        name: "amethyst",
+        type: "",
+        image: ""
+    },
+    {
+        name: "garnet",
+        type: "",
+        image: ""
+    },
+    {
+        name: "basalt",
+        type: "",
+        image: ""
+    }
+
+
+
+]
+
+function rockTemplate(rock) {
+    return `<table>
+    <tr><td>Name: ${rock.name} </td></tr>
+    <tr><td>Location: ${rock.type} </td></tr>
+    <tr><td><img src=${rock.image} alt=${rock.name}></td></tr>
+    
+  </table>`;
+
+}
+
+function renderRocks(rock) {
+    const html = rock.map(rockTemplate);
+    document.querySelector("main").innerHTML = html.join("");
+};
